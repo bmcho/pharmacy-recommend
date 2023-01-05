3c8d873378030408019b967dfc018c40b56935a3
bmcho <bmcho91@naver.com>
2023-01-06 00:49:50 +0900

    initialized setting

---
+++ src/test/java/com/recommand/pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommand.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
