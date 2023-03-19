2c0d546ea7565d6996dfadb1760c5dd63e2cbcd4
bmcho <bmcho91@naver.com>
2023-03-20 00:06:13 +0900

    feat: spring retry 적용 및 테스트코드 작성

--- src/main/java/com/recommand/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/recommand/api/service/KakaoAddressSearchService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -22,9 +25,10 @@ public class KakaoAddressSearchService {
     @Value("${kakao.rest.api.key}")
     private String kakaoRestApiKey;
 
+    @Retryable(retryFor = {RuntimeException.class}, maxAttempts = 2, backoff = @Backoff(2000))
     public KakaoApiResponseDto requestAddressSearch(String address) {
 
-        if(ObjectUtils.isEmpty(address)) return null;
+        if (ObjectUtils.isEmpty(address)) return null;
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
         HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
@@ -35,6 +39,12 @@ public class KakaoAddressSearchService {
                 HttpMethod.GET,
                 httpEntity,
                 KakaoApiResponseDto.class).getBody();
+    }
+
+    @Recover
+    public KakaoApiResponseDto recoverRequestAddressSearch(RuntimeException e, String address) {
+        log.error("[RequestAddressSearch-All retries failed] address: {}, error: {}", address, e);
+        return null;
     }
 
 }
